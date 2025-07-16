288ccc5f73513b6fbea54161f64b4bb62f444ae4
Timothy Erbert <timjerbert@gmail.com>
2025-07-16 01:26:05 -0500

    Initial project setup
    Installed Svelte. Made page routing

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="js">
+    let {children} = $props();
+</script>
+
+<nav>
+    <div class="navbar">
+        <a href="/">Home</a>
+        <a href="/projects">Home</a>
+        <a href="/contact">Contact</a>
+    </div>
+</nav>
+
+{@render children()}
+
+<style>
+    .navbar{
+      width:100%;
+      height:30px;
+      background:green;
+      display:flex;  
+    }
+
+    .navbar a{
+        background:pink;
+    }
+</style>
---
+++ frontend/src/routes/projects/+page.svelte
@@ -0,0 +1,2 @@
+<h1>This is the projects page</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
