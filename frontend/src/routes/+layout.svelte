f7df19756363d39c92576f070d14a9d41dd13102
Timothy Erbert <timjerbert@gmail.com>
2025-09-09 10:32:22 -0500

    made a site container that adjusts properly with the navbar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="js">
     import { goto } from '$app/navigation';
-    import navButtonPressed from "$lib/assets/navbar/pressed3d.png"
-    import navButtonUnpressed from "$lib/assets/navbar/unpressed3d.png"
+    import { page } from '$app/state';
     let {children} = $props();
 
 
@@ -13,55 +12,64 @@
 
 </script>
 
-<nav>
-    <div class="navbar">
-        <button class="navbar-button" onclick={() => goto('/')}>
-            <img class="pressed" src={navButtonPressed} alt="" />
-            <img class="unpressed" src={navButtonUnpressed} alt="" />
-        </button>
-        <button class="navbar-button" onclick={() => goto('/projects')}>
-            <img class="pressed" src={navButtonPressed} alt="" />
-            <img class="unpressed" src={navButtonUnpressed} alt="" />
-        </button>
-        <button class="navbar-button" onclick={() => goto('/contact')}>
-            <img class="pressed" src={navButtonPressed} alt="" />
-            <img class="unpressed" src={navButtonUnpressed} alt="" />
-        </button>
-
+    <div class="site-container">
+        <nav class="navbar">
+            <button class="navbar-button" onclick={() => goto('/')}>
+                Home
+            </button>
+            <button class="navbar-button" onclick={() => goto('/projects')}>
+                Projects
+            </button>
+            <button class="navbar-button" onclick={() => goto('/design')}>
+                Design
+            </button>
+            <button class="navbar-button hflex-last" onclick={() => goto('/contact')}>
+                Contact
+            </button>
+        </nav>
+        <div class="page-container">{@render children()}</div>
     </div>
-</nav>
 
-{@render children()}
 
 <style scoped>
+
+    .site-container{
+        display:flex;
+        flex-direction: column;
+        width:100dvw;
+        height:100dvh;
+        background:rgb(255, 255, 255)
+    }
+    .page-container{
+        display:block;
+        width:100%;
+        height:100%;
+    }
     .navbar{
-      height:131px;
       width:100%;
       display:flex;
       height:fit-content;
-      padding: 0px;
+      padding: 0;
       display:flex;  
-      background-image: url('../lib/assets/navbar/keyboardbg3d.png');
+      background:#313131;
+      overflow-y:auto;
     }
 
     .navbar-button{
-        padding:0px;
-        margin:0px;
-        border:0px;
+        font-size:x-large;
+        border:0;
+        color:white;
+        padding:16px;
         background:none;
     }
-    .navbar-button img{
+
+    .navbar-button:hover{
+      text-decoration: underline;
     }
-    .navbar-button .pressed{
-        display:none;
+    .navbar-button:active{
+      text-decoration: underline;
     }
-    .navbar-button .unpressed{
-        display:block;
-    }
-    .navbar-button:hover .pressed{
-        display:block;
-    }
-    .navbar-button:hover .unpressed{
-        display:none;
+    .hflex-last{
+        margin-left:auto;
     }
 </style>
---
+++ frontend/src/routes/design/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Design will go here!</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
